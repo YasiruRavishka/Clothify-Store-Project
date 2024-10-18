c9f577b208326b3ae7f8b13740289aa6ab1d3743
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-10-18 19:39:48 +0530

    main update.

--- src/main/java/edu/icet/Loader.java
+++ src/main/java/edu/icet/Loader.java
@@ -1,8 +1,9 @@
-package org.icet;
+package edu.icet;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Loader extends Application {
@@ -13,6 +14,11 @@ public class Loader extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/homepage.fxml"))));
+        stage.setOnCloseRequest(event-> {
+            System.exit(0);
+        });
+        stage.getIcons().add(new Image("/img/logo.png"));
+        stage.setTitle("Homepage");
         stage.show();
     }
 }
--- src/main/java/edu/icet/Main.java
+++ src/main/java/edu/icet/Main.java
@@ -1,4 +1,4 @@
-package org.icet;
+package edu.icet;
 
 public class Main {
     public static void main(String[] args) {
