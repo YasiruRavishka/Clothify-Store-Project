63ebf2570246a8f36a6da00793ad1af9c30c937b
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-10-22 20:49:43 +0530

    SupplierService feature added.

--- src/main/java/edu/icet/service/custom/SupplierBo.java
+++ src/main/java/edu/icet/service/custom/SupplierBo.java
@@ -1,6 +1,13 @@
 package edu.icet.service.custom;
 
+import edu.icet.dto.Supplier;
 import edu.icet.service.SuperBo;
+import javafx.collections.ObservableList;
 
 public interface SupplierBo extends SuperBo {
+    ObservableList<Supplier> getAll();
+    boolean addSupplier(Supplier supplier);
+    boolean updateSupplier(Supplier supplier);
+    boolean deleteSupplier(Integer id);
+    Supplier searchSupplierById(Integer id);
 }
--- src/main/java/edu/icet/service/custom/impl/SupplierBoImpl.java
+++ src/main/java/edu/icet/service/custom/impl/SupplierBoImpl.java
@@ -1,6 +1,49 @@
 package edu.icet.service.custom.impl;
 
+import edu.icet.dao.DaoFactory;
+import edu.icet.dao.custom.SupplierDao;
+import edu.icet.dto.Supplier;
+import edu.icet.entity.SupplierEntity;
 import edu.icet.service.custom.SupplierBo;
+import edu.icet.util.DaoType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.modelmapper.ModelMapper;
 
 public class SupplierBoImpl implements SupplierBo {
+    @Override
+    public ObservableList<Supplier> getAll() {
+        SupplierDao supplierDao = DaoFactory.getInstance().getDaoType(DaoType.SUPPLIER);
+        ObservableList<Supplier> suppliers = FXCollections.observableArrayList();
+        for (SupplierEntity supplierEntity : supplierDao.getAll()) {
+            suppliers.add(new ModelMapper().map(supplierEntity, Supplier.class));
+        }
+        return suppliers;
+    }
+
+    @Override
+    public boolean addSupplier(Supplier supplier) {
+        SupplierEntity entity = new ModelMapper().map(supplier, SupplierEntity.class);
+        SupplierDao supplierDao = DaoFactory.getInstance().getDaoType(DaoType.SUPPLIER);
+        return supplierDao.save(entity);
+    }
+
+    @Override
+    public boolean updateSupplier(Supplier supplier) {
+        SupplierEntity entity = new ModelMapper().map(supplier, SupplierEntity.class);
+        SupplierDao supplierDao = DaoFactory.getInstance().getDaoType(DaoType.SUPPLIER);
+        return supplierDao.update(entity);
+    }
+
+    @Override
+    public boolean deleteSupplier(Integer id) {
+        SupplierDao supplierDao = DaoFactory.getInstance().getDaoType(DaoType.SUPPLIER);
+        return supplierDao.delete(id);
+    }
+
+    @Override
+    public Supplier searchSupplierById(Integer id) {
+        SupplierDao supplierDao = DaoFactory.getInstance().getDaoType(DaoType.SUPPLIER);
+        return new ModelMapper().map(supplierDao.search(id), Supplier.class);
+    }
 }
