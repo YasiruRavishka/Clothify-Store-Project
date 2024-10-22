6b2c866e76bb912ddec843465016b43b83ac00e7
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-10-22 20:18:42 +0530

    supplier update.

--- src/main/java/edu/icet/entity/SupplierEntity.java
+++ src/main/java/edu/icet/entity/SupplierEntity.java
@@ -1,9 +1,6 @@
 package edu.icet.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +15,7 @@ public class SupplierEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String name;
     private String email;
 }
