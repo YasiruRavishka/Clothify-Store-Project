5f0ea296cc9acf47f62f952e9b0b4be74deabe35
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-10-22 20:18:19 +0530

    employee update.

--- src/main/java/edu/icet/entity/EmployeeEntity.java
+++ src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -12,6 +12,7 @@ public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String name;
     private String address;
     private String email;
