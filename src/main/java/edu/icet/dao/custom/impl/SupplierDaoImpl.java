d915a96f7b3fbb910c543eef5da9e59c633efee6
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-10-25 16:28:00 +0530

    Revert "Hibernate update."
    This reverts commit c05f7876188ad09898c503f7125b3462d4beeae9.

--- src/main/java/edu/icet/dao/custom/impl/SupplierDaoImpl.java
+++ src/main/java/edu/icet/dao/custom/impl/SupplierDaoImpl.java
@@ -2,15 +2,10 @@ package edu.icet.dao.custom.impl;
 
 import edu.icet.dao.custom.SupplierDao;
 import edu.icet.entity.SupplierEntity;
+import edu.icet.util.HibernateUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
 
@@ -72,26 +67,6 @@ public class SupplierDaoImpl implements SupplierDao {
             return entity;
         } catch (Exception e) {
             return null;
-        }
-    }
-
-    private static class HibernateUtil {
-        private static final SessionFactory session = createSession();
-        private static SessionFactory createSession() {
-            StandardServiceRegistry build = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml")
-                    .build();
-
-            Metadata metadata = new MetadataSources(build)
-                    .addAnnotatedClass(SupplierEntity.class)
-                    .getMetadataBuilder()
-                    .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
-                    .build();
-            return metadata.getSessionFactoryBuilder().build();
-        }
-
-        public static Session getSession(){
-            return session.openSession();
         }
     }
 }
